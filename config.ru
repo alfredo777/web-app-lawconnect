52b4c021f1c8d32db85de5dc7c96cad9ac975520
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2018-09-28 19:27:34 -0500

    cambios en administrador

--- config.ru
+++ config.ru
@@ -1,5 +1,7 @@
+# encoding: utf-8
+
 require 'rubygems'
 require 'sinatra'
 require './controllers/landing'
-  
+Encoding.default_external = Encoding::UTF_8  
 run Sinatra::Application
--- controllers/landing.rb
+++ controllers/landing.rb
@@ -42,14 +42,110 @@ get "/profile" do
 end
 
 get '/admin' do
+  uri = "#{CONECTIONNODE}/admin/panel"
+  response = HTTParty.get(uri)
+  @data = response  
+  @contadores = JSON.parse(@data['counters'])
+  puts @contadores
+  @clientes = JSON.parse(@data['clientes'])
+  @citas = JSON.parse(@data['citas'])
   erb :"modules/home/admin", :layout => :"layouts/admin"
 
 end
+
+get '/admin/citas' do 
+  uri = "#{CONECTIONNODE}/admin/citas"
+  response = HTTParty.get(uri)
+  @data = response  
+  @citas = JSON.parse(@data['citas'])
+
+
+  erb :"modules/home/adminparts/citas", :layout => :"layouts/admin"
+end
+
+get '/admin/citas/:id' do
+    uri = "#{CONECTIONNODE}/admin/cita?id=#{params[:id]}"
+    response = HTTParty.get(uri)
+    @data = response  
+    @cita = JSON.parse(@data['cita'])
+
+    erb :"modules/home/adminparts/vista_de_la_cita", :layout => :"layouts/admin"
+end
+
+
+get '/admin/clientes' do 
+  uri = "#{CONECTIONNODE}/admin/clientes"
+  response = HTTParty.get(uri)
+  @data = response  
+  @clientes = JSON.parse(@data['clientes'])
+
+
+  erb :"modules/home/adminparts/clientes", :layout => :"layouts/admin"
+end
+
+get '/admin/abogados' do 
+  uri = "#{CONECTIONNODE}/admin/abogados"
+  response = HTTParty.get(uri)
+  @data = response  
+  @abogados = JSON.parse(@data['abogados'])
+
+
+  erb :"modules/home/adminparts/abogados", :layout => :"layouts/admin"
+end
+
+get '/admin/abogado' do 
+  uri = "#{CONECTIONNODE}/api/law/show?token=#{params[:token]}"
+  response = HTTParty.get(uri)
+  @data = response  
+  @abogado = JSON.parse(@data['abogado'])
+
+  erb :"modules/home/adminparts/perfil_abogado", :layout => :"layouts/admin"
+end
+
+
+get '/admin/casos' do 
+  uri = "#{CONECTIONNODE}/admin/casos"
+  response = HTTParty.get(uri)
+  @data = response  
+  @casos = JSON.parse(@data['casos'])
+
+
+  erb :"modules/home/adminparts/casos", :layout => :"layouts/admin"
+end
+
+get '/admin/caso' do 
+  uri = "#{CONECTIONNODE}/admin/caso?token=#{params[:token]}"
+  response = HTTParty.get(uri)
+  @data = response  
+  @caso = JSON.parse(@data['caso'])
+
+  erb :"modules/home/adminparts/vista_del_caso", :layout => :"layouts/admin"
+end
+
+get '/admin/tickets' do 
+  #uri = "#{CONECTIONNODE}/admin/citas"
+  uri = "#{CONECTIONNODE}/admin/tickets"
+  response = HTTParty.get(uri)
+  @data = response  
+  @tickets = JSON.parse(@data['tickets'])
+
+  puts @tickets
+
+
+  erb :"modules/home/adminparts/tickets", :layout => :"layouts/admin"
+end
+
+
+
+
 
 def current_route(path)
   route = path.split('?')
   session['route'] = route
 end
+
+
+
 
 
 
